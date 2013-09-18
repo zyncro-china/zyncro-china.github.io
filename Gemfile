6d0b6071a1fbea706a7f46c2d540b1bbe3b3756a
David Ruan <ruanwz@gmail.com>
2013-09-18 10:30:45 +0800

    submit source

---
+++ Gemfile
@@ -0,0 +1,15 @@
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source 'http://rubygems.org'
+
+gem "middleman", "~> 3.1.5"
+gem "middleman-blog", "~> 3.3.0"
+gem "middleman-deploy"
+gem "middleman-disqus"
+gem "middleman-livereload", "~> 3.1.0"
+gem "middleman-syntax"
+gem "redcarpet"
+gem "middleman-google-analytics"
+
+# For feed.xml.builder
+gem "builder", "~> 3.0"
---
+++ config.rb
@@ -0,0 +1,138 @@
+###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+
+activate :blog do |blog|
+  # blog.prefix = "blog"
+  # blog.permalink = ":year/:month/:day/:title.html"
+  blog.sources = "articles/:year-:month-:day-:title.html"
+  # blog.taglink = "tags/:tag.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = ":year.html"
+  # blog.month_link = ":year/:month.html"
+  # blog.day_link = ":year/:month/:day.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/:num"
+end
+activate :directory_indexes
+
+page "/feed.xml", :layout => false
+
+###
+# Deploy settings
+###
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true
+  # Optional Settings
+  # deploy.remote = "custom-remote" # remote name or git url, default: origin
+  deploy.branch = "master" # default: gh-pages
+end
+
+activate :livereload
+activate :syntax, :line_numbers => true
+activate :google_analytics do |ga|
+  ga.tracking_id = 'UA-44086232-1'
+end
+###
+# Compass
+###
+
+# Susy grids in Compass
+# First: gem install susy
+# require 'susy'
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
+
+## OR
+#
+#set :markdown_engine, :kramdown
+
+configure :development do
+  activate :disqus do |d|
+    d.shortname = false
+  end
+end
+
+# Build-specific configuration
+configure :build do
+  activate :disqus do |d|
+    d.shortname = "zyncrochinadevblog"
+  end
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :cache_buster
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+
+  # Or use a different image path
+  # set :http_path, "/Content/images/"
+end
