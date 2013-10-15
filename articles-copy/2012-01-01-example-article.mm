9d7b81f053bd31f7854e7dd123388c097171a731
David Ruan <ruanwz@gmail.com>
2013-10-15 18:07:26 +0800

    Automated commit at 2013-10-15 10:07:26 UTC by middleman-deploy 0.1.2

---
+++ articles-copy/2012-01-01-example-article.mm
@@ -0,0 +1,22 @@
+# This is a Heading Title
+## This is smaller title
+
+1. go home
+1. cook dinner
+
+* this is a option
+* option 2
+Thisssss is an example article. You probably want to delete it and write your own articles!
+
+```ruby
+def my_cool_method(message)
+  puts message
+  return nil
+end
+```
+```javascript
+var y = function(){
+  console.log('aa')
+}
+```
+
---
+++ articles-copy/2013-10-10-zyncro-api-console.mm
@@ -0,0 +1,10 @@
+# 为什么要做这个Zyncro Console
+
+Zyncro已经有一个API
+Console，但是再做一个可以更好地体验其API的功能，而且也不难，最重要是自己做比较有趣。
+
+# Zyncro API Console
+
+<iframe src="https://apigee.com/ruanwz/embed/console/zyncro" width="100%"
+height="600" scrolling="no"></iframe>
+
